95e270cc3d5fc8b8f00839897952815ca418b430
WatonHacker <cristobalacevedom@gmail.com>
2022-11-25 00:00:11 -0300

    Correción de directorios

--- eva3_backend/settings.py
+++ eva3_backend/settings.py
@@ -15,7 +15,7 @@ import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
+TEMPLATES_DIR = os.path.join(BASE_DIR, 'eva3_backend/templates')
 
 
 # Quick-start development settings - unsuitable for production
